0a6342d31528035d393c2bd678e872766d346b0b
Bodhi <Bodhivanstraalen@gmail.com>
2025-02-14 12:35:34 +0100

    add me

---
+++ 5namen.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Create an array to store 5 names
+        string[] names = new string[5];
+
+        Console.WriteLine("Please enter 5 names:");
+
+        // Loop to get 5 names from the user
+        for (int i = 0; i < names.Length; i++)
+        {
+            Console.Write($"Enter name {i + 1}: ");
+            names[i] = Console.ReadLine();
+        }
+
+        Console.WriteLine("\nThe names you entered are:");
+
+        // Loop to display the names entered by the user
+        foreach (string name in names)
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
