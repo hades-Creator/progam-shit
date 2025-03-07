ac92882d70c4c2628e05c5db642c5499b13eea48
hades-creator <bodhivanstraalen@gmail.com>
2025-03-07 09:38:46 +0100

    hallo

---
+++ Hello.cs
@@ -0,0 +1,10 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("\tHello Word!");
+        Console.WriteLine("\nthis is my first c# program!");
+        Console.ReadLine();
+    }
+}
